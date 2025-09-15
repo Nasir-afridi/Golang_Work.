ca698944b9e5e94f4223a6032ed6d13c58981f04
Nasir-afridi <nasirwc05@gmail.com>
2025-09-15 22:53:10 +0500

    added the simple values like numbers, strings, floats, booleans.

---
+++ 2_SimpleValues/2.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// *** simple values. ***
+
+	// Integers.
+	fmt.Println(1)
+
+	// string.
+	fmt.Println("Hello Golang")
+
+	// Booleans.
+	fmt.Println(true)
+	fmt.Println(false)
+
+	// Floats.
+	fmt.Println(19.3)
+}
