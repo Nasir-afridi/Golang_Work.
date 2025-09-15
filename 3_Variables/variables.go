00450277720b3a7237537bbad5265901b2c4eacf
Nasir-afridi <nasirwc05@gmail.com>
2025-09-15 23:01:02 +0500

    variable declarations added

---
+++ 3_Variables/variables.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "golang"
+	fmt.Println(name)
+
+	// is tarah be variables bana skty hain.
+	var age = 20
+	fmt.Println(age)
+
+	var isAdult = true
+	fmt.Println(isAdult)
+
+	// shorthand syntax
+	names := "golang"
+	fmt.Println(names)
+}
