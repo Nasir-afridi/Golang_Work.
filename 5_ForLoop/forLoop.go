8829f59c96947a86db6ad6a423ea3487b3f3140e
Nasir-afridi <nasirwc05@gmail.com>
2025-09-16 21:49:11 +0500

    added the for loop,

---
+++ 5_ForLoop/forLoop.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// for -> sirf for loop he hota hai golang mein. for ki madad sy while or do while banatay hain.
+
+	// while loop.
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i++
+	}
+
+}
