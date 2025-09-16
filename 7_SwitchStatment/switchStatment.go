0a3d27166802ec6fce6430f16ae7b5ea4de7c5e5
Nasir-afridi <nasirwc05@gmail.com>
2025-09-16 22:35:43 +0500

    added the multiple condition switch

--- 7_SwitchStatment/switchStatment.go
+++ 7_SwitchStatment/switchStatment.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	// simple switch.
@@ -18,4 +21,13 @@ func main() {
 	case 5:
 		fmt.Println("Five")
 	}
+
+	// Multiple Condition switch.
+	switch time.Now().Weekday() {
+	case time.Sunday, time.Saturday:
+		fmt.Println("It's weekend")
+	default:
+		fmt.Println("It's working days.")
+	}
+
 }
