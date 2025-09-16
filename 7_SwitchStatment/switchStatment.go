0364025529fc2275957daeac1b93ccdf05c741c9
Nasir-afridi <nasirwc05@gmail.com>
2025-09-16 22:24:13 +0500

    added the simple switch

---
+++ 7_SwitchStatment/switchStatment.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// simple switch.
+
+	i := 5
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	case 4:
+		fmt.Println("Four")
+	case 5:
+		fmt.Println("Five")
+	}
+}
