348e49a28f89a9450405190a57c9ccbcbecfd94d
Nasir-afridi <nasirwc05@gmail.com>
2025-09-29 23:56:22 +0500

    added the difference b/w nil non nil and map with make

--- 10_Maps/maps.go
+++ 10_Maps/maps.go
@@ -47,4 +47,32 @@ func main() {
 	x2 := map[string]int{"price": 1000, "age": 23}
 	fmt.Println(maps.Equal(x1, x2))
 
+	// ----------------------------
+	// 1. Nil Map
+	// ----------------------------
+	// var m map[string]int
+	// - Ye sirf declare hota hai, memory allocate nahi hoti.
+	// - Hamesha nil rehta hai.
+	// - Read allowed → agar key nahi mili to default value return hoti hai.
+	// - Write allowed nahi → write karne par panic aata hai.
+
+	// ----------------------------
+	// 2. Make Wala Map
+	// ----------------------------
+	// m := make(map[string]int)
+	// - Ye non-nil hota hai (memory allocate ho jati hai).
+	// - Shuru mein empty hota hai, baad mein keys add karte ho.
+	// - Read allowed.
+	// - Write allowed.
+
+	// ----------------------------
+	// 3. Map Literal
+	// ----------------------------
+	// m := map[string]int{"Ali": 20, "Nasir": 25}
+	// - Ye bhi non-nil hota hai (memory allocate ho jati hai).
+	// - Banate waqt hi key-value pairs dene padte hain.
+	// - Baad mein aur keys add/update kar sakte ho.
+	// - Read allowed.
+	// - Write allowed.
+
 }
